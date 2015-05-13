dfa7d968dd01b4cd07fb2634290e3fa34845c803
Ivan Vendrov <ivendrov@gmail.com>
2015-05-13 17:43:41 -0400

    Added optional Nesterov momentum to rmsprop

--- rmsprop.lua
+++ rmsprop.lua
@@ -9,10 +9,12 @@ ARGS:
 - 'config.learningRate'      : learning rate
 - 'config.alpha'             : smoothing constant
 - 'config.epsilon'           : value with which to inistialise m
+- 'config.momentum'          : value of Nesterov momentum to use
 - 'state'                    : a table describing the state of the optimizer;
                                after each call the state is modified
 - 'state.m'                  : leaky sum of squares of parameter gradients,
 - 'state.tmp'                : and the square root (with epsilon smoothing)
+- 'state.v'                  : velocity vector
 
 RETURN:
 - `x`     : the new x vector
@@ -27,10 +29,25 @@ function optim.rmsprop(opfunc, x, config, state)
     local lr = config.learningRate or 1e-2
     local alpha = config.alpha or 0.99
     local epsilon = config.epsilon or 1e-8
+    local momentum = config.momentum or 0
+    
+    -- (0) make Nesterov momentum update (BEFORE evaluation)
+    if momentum then
+      -- initialize velocity vector
+      if not state.v then
+        state.v = torch.Tensor():typeAs(x):resizeAs(x):zero()
+      end
+      x:add(momentum, state.v)
+    end
 
     -- (1) evaluate f(x) and df/dx
     local fx, dfdx = opfunc(x)
-
+    
+    if momentum then
+      -- undo Nesterov step
+      x:add(-momentum, state.v)
+    end
+      
     -- (2) initialize mean square values and square gradient storage
     if not state.m then
       state.m = torch.Tensor():typeAs(x):resizeAs(dfdx):zero()
@@ -40,10 +57,20 @@ function optim.rmsprop(opfunc, x, config, state)
     -- (3) calculate new (leaky) mean squared values
     state.m:mul(alpha)
     state.m:addcmul(1.0-alpha, dfdx, dfdx)
-
+    
     -- (4) perform update
     state.tmp:sqrt(state.m):add(epsilon)
-    x:addcdiv(-lr, dfdx, state.tmp)
+    
+    if momentum then
+      -- update velocity
+      state.v:mul(momentum)
+      state.v:addcdiv(-lr, dfdx, state.tmp)
+      
+      -- update position
+      x:add(state.v)
+    else
+      x:addcdiv(-lr, dfdx, state.tmp)
+    end
 
     -- return x*, f(x) before optimization
     return x, {fx}
